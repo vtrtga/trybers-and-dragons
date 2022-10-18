cbc75ae3f44f66a20b3560a580638aca90106e80
vtrtga <vitor.ypvalim@gmail.com>
2022-10-18 17:00:19 -0400

    Archetype class implementation

---
+++ src/Archetypes/Archetype.ts
@@ -0,0 +1,27 @@
+import { EnergyType } from '../Energy';
+
+export default abstract class Archetype {
+  private _name: string;
+  private _special: number;
+  private _cost: number;
+
+  constructor(readonly name: string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
+  }
+
+  get special() {
+    return this._special;
+  }
+
+  get cost() {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
+}
